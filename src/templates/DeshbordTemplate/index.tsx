79bbd0041fce19184e8e2c171bfa305528304db4
guusfelix2015 <gusfelix2015@gmail.com>
2022-11-10 21:02:19 -0300

    Test: testing chart

--- src/templates/DeshbordTemplate/index.tsx
+++ src/templates/DeshbordTemplate/index.tsx
@@ -1,13 +1,48 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Legend,
+} from "chart.js";
 
 import * as Styles from "./styles";
 
 const HomeTemplate = () => {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Legend,
+  );
+
+  const data = {
+    labels: ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"],
+    datasets: [
+      {
+        label: "TOTAL DE FÓRMULAS",
+        data: [400, 300, 350, 200, 280],
+        borderColor: "blue",
+        backgroundColor: "white",
+      },
+    ],
+  };
   return (
     <Layout>
       <Styles.ContainerMain>
-        <h1>Deshbord</h1>
+        <Styles.Container>
+          <Line
+            data={data}
+            options={{
+              responsive: true,
+            }}
+          />
+        </Styles.Container>
       </Styles.ContainerMain>
     </Layout>
   );
