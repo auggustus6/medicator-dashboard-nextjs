1d59961688060c93ec7aeb338ba23073e0034690
guusfelix2015 <gusfelix2015@gmail.com>
2022-11-12 12:41:11 -0300

    Fix: remove charts

--- src/templates/DashboardTemplate/index.tsx
+++ src/templates/DashboardTemplate/index.tsx
@@ -1,53 +1,10 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import { Line } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Legend,
-} from "chart.js";
 
 import * as Styles from "./styles";
-import CircleDashboard from "../../components/CircleDashboard";
 
 const DashboardTemplate = () => {
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Legend,
-  );
-
-  const data = {
-    labels: ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"],
-    datasets: [
-      {
-        label: "TOTAL DE FÓRMULAS",
-        data: [400, 300, 350, 200, 280],
-        borderColor: "blue",
-        backgroundColor: "white",
-      },
-    ],
-  };
-  return (
-    <Layout>
-      <Styles.ContainerMain>
-        <Styles.Container>
-          <Line
-            data={data}
-            options={{
-              responsive: true,
-            }}
-          />
-          <CircleDashboard />
-        </Styles.Container>
-      </Styles.ContainerMain>
-    </Layout>
-  );
+  return <Layout>Helo</Layout>;
 };
 
 export default DashboardTemplate;
