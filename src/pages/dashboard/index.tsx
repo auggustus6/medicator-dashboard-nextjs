c487d799d840760a45a7c7b343db7930b2ac44fb
guusfelix2015 <gusfelix2015@gmail.com>
2022-11-12 15:39:51 -0300

    Chore: header

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,10 @@
-import Layout from "../../components/Layout";
 import DashboardTemplate from "../../templates/DashboardTemplate/index";
 
 const Dashboard = () => {
   return (
-    <Layout>
+    <>
       <DashboardTemplate />
-    </Layout>
+    </>
   );
 };
 
