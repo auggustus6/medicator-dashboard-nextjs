89b801c4fc3c3bda1f1766a4e4ee560537dd562b
guusfelix2015 <gusfelix2015@gmail.com>
2022-11-12 12:03:12 -0300

    Fix: revert commit

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout";
+import DashboardTemplate from "../../templates/DashboardTemplate/index";
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <DashboardTemplate />
+    </Layout>
+  );
+};
+
+export default Dashboard;
