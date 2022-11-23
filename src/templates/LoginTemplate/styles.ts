e20fdf5070b4e6d5b404d6bd099c0f96c0117bdb
guusfelix2015 <gusfelix2015@gmail.com>
2022-11-23 20:07:40 -0300

    chore: styles page login

--- src/templates/LoginTemplate/styles.ts
+++ src/templates/LoginTemplate/styles.ts
@@ -1,16 +1,108 @@
 import styled from "styled-components";
 
-export const Intro = styled.div`
+export const ContainerIntro = styled.div`
   border-radius: 8px;
   box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.25);
   display: flex;
   justify-content: center;
+  align-items: center;
   width: 100vw;
   height: 100vh;
+
+  @media (max-width: 425px) {
+    img {
+      display: none;
+    }
+  }
 
   img {
     max-width: 50%;
     width: 100%;
+    border-radius: 8px;
+  }
+`;
+
+export const Icon = styled.div`
+  width: 10rem;
+  height: 10rem;
+  background: #06c575;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  max-width: 40rem;
+  width: 100%;
+  flex-direction: column;
+  margin-top: 3rem;
+
+  @media (max-width: 1024px) {
+    max-width: 30rem;
+  }
+
+  @media (max-width: 768px) {
+    max-width: 25rem;
+  }
+
+  @media (max-width: 425px) {
+    width: 40rem;
+  }
+
+  input {
+    padding: 1rem;
+    outline: none;
+    width: 100%;
+    border-radius: 8px;
+    font-weight: 500;
+    font-weight: 500;
+    font-size: 1.8rem;
+    margin-bottom: 2rem;
+    border: 1px solid gray;
+  }
+
+  button {
+    padding: 1.6rem 3.2rem;
+    border-radius: 50px;
+    font-size: 2.4rem;
+    line-height: 3.2rem;
+    font-weight: 500;
+    background: #04ab65;
+    color: #fff;
+    cursor: pointer;
+    outline: none;
+    border: none;
+  }
+
+  button:hover {
+    background: #06c575;
+  }
+`;
+
+export const Box = styled.div`
+  max-width: 100rem;
+  width: 100%;
+  min-height: 50rem;
+  display: flex;
+  border-radius: 8px;
+  box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.25);
+
+  @media (max-width: 1024px) {
+    max-width: 80rem;
+  }
+
+  @media (max-width: 768px) {
+    max-width: 60rem;
+  }
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+    height: 30rem;
+    align-items: center;
+    justify-content: center;
+    width: 30rem;
   }
 `;
 
