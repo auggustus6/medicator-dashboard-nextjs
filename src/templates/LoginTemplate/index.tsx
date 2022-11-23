c2f2c6d36b6e54632953ad2a42a15f6fed5139e2
guusfelix2015 <gusfelix2015@gmail.com>
2022-11-23 20:07:40 -0300

    chore: styles page login

--- src/templates/LoginTemplate/index.tsx
+++ src/templates/LoginTemplate/index.tsx
@@ -3,14 +3,21 @@ import { UserCircle } from "phosphor-react";
 
 const Login = () => {
   return (
-    <Styles.Intro>
-      <img src="/img/bg.png" alt="" />
-      <Styles.Fomulary>
-        <UserCircle size={80} />
-        <input type="text" />
-        <input type="password" />
-      </Styles.Fomulary>
-    </Styles.Intro>
+    <Styles.ContainerIntro>
+      <Styles.Box>
+        <img src="/img/bg.png" alt="" />
+        <Styles.Fomulary>
+          <Styles.Icon>
+            <UserCircle size={80} color="#fff" />
+          </Styles.Icon>
+          <Styles.Form>
+            <input type="email" placeholder="E-mail Adrress" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+          </Styles.Form>
+        </Styles.Fomulary>
+      </Styles.Box>
+    </Styles.ContainerIntro>
   );
 };
 
